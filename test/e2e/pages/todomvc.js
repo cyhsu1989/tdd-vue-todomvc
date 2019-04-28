32fb09afcc8e777158e2509104524339b9bc0dd3
CHIN-YU HSU <cyhsu1989@gmail.com>
2019-04-28 16:18:07 +0800

    整理重複的 selector 存為變數，提升日後更動元素後的調整便利性。

--- test/e2e/pages/todomvc.js
+++ test/e2e/pages/todomvc.js
@@ -1,4 +1,7 @@
 module.exports = function (browser) {
+  const newTodo = '.new-todo';
+  const firstTodoItem = '.todo-list li:first-child';
+
   this.show = () => browser
     .url('http://localhost:8080')
     .waitForElementVisible('#app', 5000);
@@ -6,17 +9,17 @@ module.exports = function (browser) {
   this.shouldSeeTitleAndInput = () => browser
     .assert.elementPresent('.header')
     .assert.containsText('h1', 'todos')
-    .assert.elementPresent('.new-todo');
+    .assert.elementPresent(newTodo);
 
   this.addTodo = todo => browser
-    .setValue('.new-todo', [todo, browser.Keys.ENTER]);
+    .setValue(newTodo, [todo, browser.Keys.ENTER]);
 
   this.shouldSeeAtNewTodo = value => browser
-    .assert.value('.new-todo', value);
+    .assert.value(newTodo, value);
 
   this.shouldSeeAtFirstItem = todo => browser
-    .waitForElementVisible('.todo-list li:first-child', 1000)
-    .assert.containsText('.todo-list li:first-child > .view > label', todo);
+    .waitForElementVisible(firstTodoItem, 1000)
+    .assert.containsText(`${firstTodoItem} > .view > label`, todo);
 
   this.shouldHiddenTodoListWithoutTodo = () => browser
     .assert.hidden('.main', 1000);
